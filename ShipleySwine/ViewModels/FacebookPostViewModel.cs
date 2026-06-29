deb4a2a322a06dea66ec13801a7d0228f8668b30
Nicholas Richardson <Nicholas.richardson.05@gmail.com>
2026-06-29 11:20:27 -0400

    test

---
+++ ShipleySwine/ViewModels/FacebookPostViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ShipleySwine.ViewModels
+{
+    public class FacebookPostViewModel
+    {
+        public string Message { get; set; }
+        public DateTime CreatedTime { get; set; }
+        public string Permalink { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
